43bf3df01530239003a3b7ef09c5e220ee964dda
rishavroy97 <rishavroy008@gmail.com>
2024-06-27 15:28:35 -0400

    Add Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,24 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-const page = () => {
-    return <p>Customers Page</p>;
+const page = async ({ searchParams }: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) => {
+
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+    return (
+        <CustomersTable customers={customers} />
+    );
 };
 
 export default page;
